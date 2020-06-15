27e389f8578313493161b96c8c87720d9e36f9db
MaristelaP <marispeg@gmail.com>
2020-06-14 21:22:48 -0300

    feat: acrescentado informação no alerta

--- src/js/index.js
+++ src/js/index.js
@@ -2,6 +2,6 @@ import Swal from 'https://cdn.jsdelivr.net/npm/sweetalert2@8/src/sweetalert2.js'
 
 Swal.fire(
     '',
-    'Este site faz parte de projeto acadêmico e não contém fins lucrativos',
+    'Este site faz parte de projeto acadêmico da turma FLX1554 Curso de Análise e Desenvolvimento de Sistemas - Centro Universitário Leonardo Da Vince e não contém fins lucrativos',
     'info'
 )
