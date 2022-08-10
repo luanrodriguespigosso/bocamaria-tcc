fcdbc3721d102bdc93bd2bd2be44fef6f6f28852
luan <luan_naul90@hotmail.com>
2022-08-10 17:04:20 -0300

    style: Alterado a unidade de medida de px para rem, adicionado variaveis css e nova fonte

--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,7 @@
-import Swal from 'https://cdn.jsdelivr.net/npm/sweetalert2@8/src/sweetalert2.js'
+// import Swal from 'https://cdn.jsdelivr.net/npm/sweetalert2@8/src/sweetalert2.js'
 
-Swal.fire(
-    '',
-    'Este site faz parte de um projeto acadêmico da turma FLX1554 Curso de Análise e Desenvolvimento de Sistemas - Centro Universitário Leonardo Da Vince e não contém fins lucrativos',
-    'info'
-)
+// Swal.fire(
+//     '',
+//     'Este site faz parte de um projeto acadêmico da turma FLX1554 Curso de Análise e Desenvolvimento de Sistemas - Centro Universitário Leonardo Da Vince e não contém fins lucrativos',
+//     'info'
+// )
