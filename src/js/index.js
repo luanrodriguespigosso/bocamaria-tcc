dc097b44c105674133ed76b6b085a7c1c8db72e1
luan <luan_naul90@hotmail.com>
2022-08-20 18:06:20 -0300

    feat: Carousel cards lançamentos

--- src/js/index.js
+++ src/js/index.js
@@ -5,3 +5,18 @@
 //     'Este site faz parte de um projeto acadêmico da turma FLX1554 Curso de Análise e Desenvolvimento de Sistemas - Centro Universitário Leonardo Da Vince e não contém fins lucrativos',
 //     'info'
 // )
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: 4,
+  spaceBetween: 10,
+  slidesPerGroup: 4,
+  loop: true,
+  looFillGroupWithBlank: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
